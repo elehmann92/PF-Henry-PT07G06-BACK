0f0f1e225bdce0e0c625bfb496b7831d1630e269
Eric Lehmann <equilehmann92@gmail.com>
2022-10-20 22:07:46 -0300

    Cart Router setup

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 const productsRouter = require("./products");
 const categoriesRouter = require("./categories");
 const usersRouter = require("./users");
+const cartRouter = require("./cart");
 
 const router = Router();
 
 router.use("/products", productsRouter);
 router.use("/categories", categoriesRouter);
 router.use("/users", usersRouter);
+router.use("/cart", cartRouter);
 
 module.exports = router;
