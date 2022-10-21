795d343171f17a5878cd29d9fa91797258d33c70
Eric Lehmann <equilehmann92@gmail.com>
2022-10-21 15:33:27 -0300

    Agrego Cambio de Marian (para descartar su rama)

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -11,7 +11,7 @@ module.exports = (sequelize) => {
         autoIncrement: true,
       },
       name: {
-        type: DataTypes.STRING,
+        type: DataTypes.STRING(10000),
         allowNull: false,
       },
       price: {
