0652d32329ecb8757e3a424f37c9d4af64b6955e
Eric Lehmann <equilehmann92@gmail.com>
2022-10-23 16:28:09 -0300

    SPAM Limpiado de Categories. Vuelvo a Alter: true

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const {PORT}= require('./src/config')
 const {conn} = require('./src/db.js')
 const {createUsers, creatCategories, createProducts} = require('./dbSetup/idex')
 // // Syncing all the models at once.
- conn.sync({ force: true }).then(() => {
+ conn.sync({ alter: true }).then(() => {
      server.listen(PORT, () => {
        console.log('Server listening'); // eslint-disable-line no-console
 
