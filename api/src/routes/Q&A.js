e7b6d964da1b5940caa6c4e8fe27c3c88d081cde
Eric Lehmann <equilehmann92@gmail.com>
2022-10-31 15:37:11 -0300

    ruta post agregada en reviews y qanda

--- api/src/routes/Q&A.js
+++ api/src/routes/Q&A.js
@@ -1,18 +1,40 @@
-const {Router}= require("express");
+const { Router } = require("express");
 
-const {QandA}= require("../db");
+const { QandA } = require("../db");
 
-const router= Router();
+const router = Router();
 
-router.get("/", async (req,res)=>{
-    try{
-        const qA= await QandA.findAll({order:["id"]}); 
+router
+  .get("/", async (req, res) => {
+    try {
+      const qA = await QandA.findAll({ order: ["id"] });
 
-        return res.status(200).json(qA);
+      return res.status(200).json(qA);
+    } catch (error) {
+      return res.status(404).json(error);
     }
-    catch(error){
-        return res.status(404).json(error);
-    }
-})
+  })
 
-module.exports= router;
+  .post("/", async (req, res) => {
+    const { question, productQAndAId } = req.body;
+    // **Hardcodeado** hasta que incorporemos token
+    const askerId = 8;
+    try {
+        if (!question || !productQAndAId) throw new Error('No question or product id where recieved')
+
+        const newQAndABlock = await QandA.create({
+            question,
+        })
+
+        console.log(newQAndABlock)
+        
+        await newQAndABlock.setAsker(askerId);
+        await newQAndABlock.setProductQAndA(productQAndAId);
+
+        res.json(newQAndABlock)
+
+    } catch (error) {
+        res.status(400).json(error.message)
+    }
+  });
+module.exports = router;
--- api/src/routes/reviews.js
+++ api/src/routes/reviews.js
@@ -1,10 +1,12 @@
 const {Router}= require("express");
 
 const {Reviews}= require("../db");
+const { getProductById } = require("../handlers");
 
 const router= Router();
 
-router.get("/", async (req,res)=>{
+router
+.get("/", async (req,res)=>{
     try{
         const reviews= await Reviews.findAll({order:["id"]});
 
@@ -14,5 +16,31 @@ router.get("/", async (req,res)=>{
         return res.status(404).json(error);
     }
 })
+.post("/",async (req,res) => {
+    const {stars, productReviewed} = req.body
+    // **Hardcodeado** hasta que incorporemos token
+    const reviewer = 8;
+    try {
+        const product = await getProductById(productReviewed)
+        const owner = product.toJSON().ownerId
+        if (!product || !owner) throw new Error ('Product or owner wasnt found') 
+        
+        const review = await Reviews.create({
+            reviewerId: reviewer,
+            stars,
+        })
+        
+        await review.setProductReviewed(productReviewed)
+        await review.setUserReviewed(owner)
+
+        res.json(review)
+
+    } catch (error) {
+        res.status(400).json(error.message)
+    }
+
+
+})
+
 
 module.exports= router;
