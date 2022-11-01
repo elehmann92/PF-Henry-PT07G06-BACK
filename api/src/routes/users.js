230b7f3907f73569039f53fa121a9f827ecded5b
Eric Lehmann <equilehmann92@gmail.com>
2022-11-01 20:08:49 -0300

    Rutas USERS protegidas

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { getUsersDb, getUserById } = require("../handlers");
+const { getUsersDb, getUserById, throwError } = require("../handlers");
 const {
   getRole
 } = require("../handlers/routeProtection");
@@ -9,22 +9,22 @@ const router = Router();
 router
   .get("/", getRole, async (req, res) => {
     try {
-      if(req.role !== 'admin'){ throw new Error('You are not an admin')}
+      if(req.role !== 'admin') throwError('You are not an Admin', 401)
       const usersDb = await getUsersDb();
       res.json(usersDb);
     } catch (error) {
-      res.status(404).json(error.message);
+      res.status(error.number || 400).json(error.message);
     }
   })
 
   .get("/unique", getRole, async (req, res) => {
     const id  = req.id;
     try {
-      if(req.role === "guest"){ throw new Error('You are not signed in')}
+      if(req.role === "guest") throwError('You are not signed in', 401)
       const user = await getUserById(id);
       res.json(user);
     } catch (error) {
-      res.status(404).json(error.message)
+      res.status(error.number || 400).json(error.message)
     }
   })
 
@@ -56,9 +56,8 @@ router
     const idToken = req.id;
 
     try {
-      if (!body || !Object.keys(body).length) {
-        throw new Error("No data provided. Nothing to update");
-      }
+      if (!body || !Object.keys(body).length) throwError("No data provided. Nothing to update", 400)
+    
 
       if(role === 'admin' || parseInt(id) === parseInt(idToken)) {
 
@@ -71,7 +70,7 @@ router
       }
       res.status(401).json('Unauthorized');
     } catch (error) {
-      res.status(400).json(error.message);
+      res.status(error.number || 400).json(error.message);
     }
   });
 
