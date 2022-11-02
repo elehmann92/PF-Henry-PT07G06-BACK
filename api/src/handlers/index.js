f7b8a6b2268d5fa897e1361483395fe75d1b0dd9
Eric Lehmann <equilehmann92@gmail.com>
2022-11-01 23:18:27 -0300

    Rutas Cart y Favorites Protegidas

--- api/src/handlers/index.js
+++ api/src/handlers/index.js
@@ -20,16 +20,28 @@ async function getUsersDb() {
     order: ["id"],
     include: [
       {
-        model: Cart,
-        as: "cartUser",
-      },
-      {
         model: Product,
         as: "productsOwner",
       },
       {
+        model: Cart,
+        as: "cartUser",
+        include: {
+          model: Product,
+          through: {
+            attributes: [],
+          },
+        },
+      },
+      {
         model: Favorites,
         as: "favoritesUser",
+        include: {
+          model: Product,
+          through: {
+            attributes: [],
+          },
+        },
       },
       {
         model: Reviews,
@@ -260,7 +272,7 @@ async function getCartsDb() {
 }
 
 async function getCartById(id) {
-  if (!id) throw new Error("Missing Id");
+  if (!id) throwError("Missing Id",401);
   const cart = await Cart.findByPk(id, {
     include: {
       model: Product,
@@ -270,17 +282,17 @@ async function getCartById(id) {
     },
   });
 
-  if (!cart) throw new Error("No Cart matches the provided ID");
+  if (!cart) throwError("No Cart matches the provided ID",404);
 
   return cart;
 }
 
 async function findCartAndProduct(cartId, productId) {
-  const cartToAddTo = await getCartById(cartId);
+  const cartToModify = await getCartById(cartId);
 
-  const productToAdd = await getProductById(productId);
+  const productToModify = await getProductById(productId);
 
-  return { cartToAddTo, productToAdd };
+  return { cartToModify, productToModify };
 }
 
 async function getFavoritesDb() {
@@ -311,11 +323,11 @@ async function getFavoritesById(id) {
 }
 
 async function findFavoritesAndProduct(favListId, productId) {
-  const favListToAddTo = await getFavoritesById(favListId);
+  const favListToAddModify = await getFavoritesById(favListId);
 
-  const productToAdd = await getProductById(productId);
+  const productToModify = await getProductById(productId);
 
-  return { favListToAddTo, productToAdd };
+  return { favListToAddModify, productToModify };
 }
 
 async function getShoppingOrderListWithDetails() {
