7ce540395a8589c115a5390695eaa2497bd00e66
Eric Lehmann <equilehmann92@gmail.com>
2022-11-05 11:10:41 -0300

    Rutas transactions casi protegidas. Falta bytoken

--- api/src/routes/transactions.js
+++ api/src/routes/transactions.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
+const { Op } = require("sequelize");
 const { Transaction, Balance } = require("../db");
-const { getTransactions, getInstanceById} = require("../handlers");
+const { getTransactions, getInstanceById, throwError} = require("../handlers");
 const { getRole } = require("../handlers/routeProtection");
 
 const router = Router();
@@ -12,28 +13,58 @@ router.get("/", getRole, async (req, res) => {
     const transactions = await getTransactions()
     res.json(transactions)
   } catch (error) {
-    res.status(400).json(error.message)
+    res.status(error.number || 400).json(error.message)
   }
 })
 
-.get('/:id', async (req, res) => {
-  const {id} = req.params;
+.get('/:transactionId', getRole,async (req, res) => {
+  const {transactionId} = req.params;
+  const {role, id} = req
   try {
-    const transaction = await getInstanceById(Transaction,id)
+    if(role === 'guest') throwError('You are not Authorized', 401)
+
+    const transaction = await getInstanceById(Transaction,transactionId)
+    const {sellerId, buyerId} = transaction
+    if (role === 'user' && sellerId !== parseInt(id) && buyerId !== parseInt(id)) throwError('You are not Authorized', 401)
+
     res.json(transaction)
   } catch (error) {
-    res.status(400).json(error.message)
+    res.status(error.number || 400).json(error.message)
   }
 })
 
-.put("/:id", getRole, async (req,res) => {
-  const {id} = req.params;
-  const body = req.body
-  try {
-    const {role} = req
-    if(role !== 'admin') throwError('You are not Authorized', 401)
-    const transactionToUpdate = await getInstanceById(Transaction,id);
+// .get('/byToken', getRole, async (req, res) => {
+//   /* RESPONDER CON UN OBJETO JSON
+//   const transactions = {
+//     asBuyer: [{},{}],
+//     asSeller: [{},{}]
+//   }; */
+//   const {role} = req;
+//   const id = parseInt(req.id)
+//   try {
+//     if (role === 'guest') throwError('You are not Authorized', 401)
+//     const userTrasactions = await Transaction.findAll({where:{
+//       [Op.or]:{
+//         buyerId: id
+//       }
+//     }})
+//   } catch (error) {
     
+//   }
+  
+// })
+
+
+.put("/:transactionId", getRole, async (req,res) => {
+  const {transactionId} = req.params;
+  const body = req.body
+  const {role, id} = req
+  try {
+    if (!body || !Object.keys(body)?.length) throwError('No info provided to update', 400)
+    if(role === 'guest') throwError('You are not Authorized', 401)
+    const transactionToUpdate = await getInstanceById(Transaction,transactionId);
+    const {sellerId, buyerId} = transactionToUpdate
+    if (role === 'user' && sellerId !== parseInt(id) && buyerId !== parseInt(id)) throwError('You are not Authorized', 401)
     const updated = transactionToUpdate.set(body)
     
     await transactionToUpdate.save()
@@ -46,7 +77,7 @@ router.get("/", getRole, async (req, res) => {
     res.json(updated)
 
   } catch (error) {
-    res.status(400).json(error.message)
+    res.status(error.number || 400).json(error.message)
   }
 })
 
