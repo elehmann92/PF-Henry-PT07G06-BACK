3f254ba2aaa630d8f9832a6e82510cd8d5812cd6
Eric Lehmann <equilehmann92@gmail.com>
2022-11-05 17:21:54 -0300

    Rutas Q&A protegidas y sumado get /bytoken

--- api/src/routes/Q&A.js
+++ api/src/routes/Q&A.js
@@ -1,65 +1,104 @@
 const { Router } = require("express");
 
-const { QandA } = require("../db");
-const { getInstanceById } = require("../handlers");
+const { QandA, Product } = require("../db");
+const { getInstanceById, throwError } = require("../handlers");
+const { getRole } = require("../handlers/routeProtection");
 
 const router = Router();
 
 router
   .get("/", async (req, res) => {
     try {
-      const qA = await QandA.findAll({ order: ["id"] });
+      const qA = await QandA.findAll({ order: ["id"] , include:{
+        model: Product,
+        as: "productQAndA"
+      }});
 
       return res.status(200).json(qA);
     } catch (error) {
-      return res.status(404).json(error);
+      return res.status(404).json(error.message);
+    }
+  })
+
+  .get("/byToken",getRole, async (req,res) => {
+    const {role, id} = req
+    try {
+      if (role === 'guest') throwError('You are not signed in', 401)
+      const qAndAs = await QandA.findAll({include:{
+        model: Product,
+        as: "productQAndA"
+      }})
+      
+      const asAsker = qAndAs.filter(ele => ele.askerId === id);
+      const asOwner = qAndAs.filter(ele => ele.productQAndA?.ownerId === id);
+    
+      res.json({
+        userQAndAs: {
+          asAsker,
+          asOwner,
+        },
+      });
+    } catch (error) {
+      res.status(error.number || 400).json(error.message);
     }
   })
 
   .get("/:id", async (req, res) => {
     const { id } = req.params;
     try {
-      const qAndABlock = await getInstanceById(QandA, id);
+      const qAndABlock = await QandA.findByPk(id, {
+        include: {
+          model: Product,
+          as: "productQAndA",
+        },
+      });
       res.json(qAndABlock);
     } catch (error) {
       res.status(400).json(error.message);
     }
   })
 
-  .post("/", async (req, res) => {
+  .post("/", getRole,async (req, res) => {
     const { question, productQAndAId } = req.body;
-    // **Hardcodeado** hasta que incorporemos token
-    const askerId = 8;
+    const {role, id} = req
     try {
-      if (!question || !productQAndAId)
-        throw new Error("No question or product id where received");
+      if (role === 'guest') throwError('You are not signed in', 401)
+      if (!question || !productQAndAId) throwError("No question or product id where received", 400);
 
       const newQAndABlock = await QandA.create({
         question,
       });
 
-      console.log(newQAndABlock);
-
-      await newQAndABlock.setAsker(askerId);
+      await newQAndABlock.setAsker(id);
       await newQAndABlock.setProductQAndA(productQAndAId);
+
+      // **PENDIENTE** -> DESPACHAR MAIL CON PREGUNTA AL USUARIO
 
       res.json(newQAndABlock);
     } catch (error) {
-      res.status(400).json(error.message);
+      res.status(error.number || 400).json(error.message);
     }
   })
 
-  .put("/updateAnswer/:id", async (req, res) => {
-    const { id } = req.params;
+  .put("/updateAnswer/:qAndABlockId", getRole,async (req, res) => {
+    const { qAndABlockId } = req.params;
     const { answer } = req.body;
+    const {role, id} = req
     try {
-      if (!answer)
-        throw new Error("Update failed. No answer text was received");
-      const qAndABlock = await getInstanceById(QandA, id);
+      if (role === "guest") throwError("You are not signed in", 401);
+      if (!answer) throwError("Update failed. No answer text was received", 400);
+      const qAndABlock = await QandA.findByPk(qAndABlockId, {
+        include: { model: Product, as: "productQAndA" },
+      });
+      if (role === 'user' && parseInt(id) !== qAndABlock.toJSON()?.productQAndA?.ownerId)
+        throwError(
+          "You can not update answers to questions which you do not own",
+          401
+        );
       const updated = await qAndABlock.update({ answer });
       res.json(updated);
     } catch (error) {
-      res.status(400).json(error.message);
+      res.status(error.number || 400).json(error.message);
     }
   });
 module.exports = router;
