6d6e774df7018b2bcac02a958d4dbac5160ca26b
Eric Lehmann <equilehmann92@gmail.com>
2022-11-06 16:50:28 -0300

    Rutas Reviews protegidas
    Solo quien haya comprado un producto puede submitear una review.
    Se controla trayendo el modelo user incluyendo model Transaction. Si el producto existe en ese listado, autoriza la review

--- api/src/routes/reviews.js
+++ api/src/routes/reviews.js
@@ -1,28 +1,42 @@
 const { Router } = require("express");
 
-const { Reviews } = require("../db");
-const { getProductById, updateUserRating } = require("../handlers");
+const { Reviews, User, Transaction } = require("../db");
+const { getProductById, updateUserRating, throwError } = require("../handlers");
+const {getRole} = require('../handlers/routeProtection')
 
 const router = Router();
 
 router
-  .get("/", async (req, res) => {
+  .get("/",async (req, res) => {
+    const {role, id} = req
     try {
       const reviews = await Reviews.findAll({ order: ["id"] });
 
       return res.status(200).json(reviews);
     } catch (error) {
-      return res.status(404).json(error);
+      return res.status(error.number||400).json(error.message);
     }
   })
-  .post("/", async (req, res) => {
+  .post("/",getRole, async (req, res) => {
     const { stars, productReviewed } = req.body;
-    // **Hardcodeado** hasta que incorporemos token
-    const reviewer = 8;
+    const reviewer = req.id
+    const {role} = req
     try {
+      if (role === 'guest') throwError('You are not signed in', 401)
+      const userWithTransactions = await User.findByPk(reviewer, {
+        include:{
+          model: Transaction,
+          as: "buyer"
+        }
+      })
+
+      const userBoughtProduct = userWithTransactions.toJSON().buyer.some(ele=> ele.productId === productReviewed)
+
+      if (!userBoughtProduct) throwError('Solo puedes publicar una review sobre un producto que hayas comprado', 401)
+
       const product = await getProductById(productReviewed);
       const owner = product.toJSON().ownerId;
-      if (!product || !owner) throw new Error("Product or owner wasnt found");
+      if (!product || !owner) throwError("Product or owner wasnt found", 404);
 
       const review = await Reviews.create({
         reviewerId: reviewer,
@@ -34,9 +48,11 @@ router
 
       await updateUserRating(owner)
 
+      // **PENDIENTE** -> DESPACHAR MAIL AL OWNER AVISANDO QUE RECIBIÓ UNA REVIEW
+
       res.json(review);
     } catch (error) {
-      res.status(400).json(error.message);
+      res.status(error.number || 400).json(error.message);
     }
   });
 
